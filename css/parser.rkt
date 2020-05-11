b821e325834afeebccaa3846fe6e8a68198cae5a
Sage Gerard <sage@sagegerard.com>
2020-05-11 17:24:04 -0400

    Move main.rkt to parser.rkt

--- css/parser.rkt
+++ css/parser.rkt
@@ -1,7 +1,5 @@
 #lang racket/base
 
-(require "preprocess.rkt")
-
 ; Follows §3, except it assumes UTF-8.
 (define (parse-css in)
-  (construct-tree (tokenize-css in)))
+  (construct-tree (tokenize in)))
