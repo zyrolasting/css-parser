82437ccdd403c119fa4d7fe936821ebdee018ff4
Sage Gerard <sage@sagegerard.com>
2020-05-11 18:34:30 -0400

    Use code point names in terms.rkt

--- css/tokenizer/terms.rkt
+++ css/tokenizer/terms.rkt
@@ -1,6 +1,10 @@
 #lang racket/base
 
+; Predicates derived from §4.2
+
 (provide (all-defined-out))
+
+(require "code-points.rkt")
 
 (define (surrogate-code-point? code-point)
   (and (>= code-point #xD800)
@@ -8,13 +12,49 @@
 
 (define (hex-digit? ch)
   (and (char? ch)
-       (or (and (char>=? ch #\A)
-                (char<=? ch #\F))
-           (and (char>=? ch #\a)
-                (char<=? ch #\f))
+       (or (and (char>=? ch LATIN-CAPITAL-LETTER-A)
+                (char<=? ch LATIN-CAPITAL-LETTER-F))
+           (and (char>=? ch LATIN-SMALL-LETTER-A)
+                (char<=? ch LATIN-SMALL-LETTER-F))
            (digit? ch))))
 
 (define (digit? ch)
   (and (char? ch)
-       (char>=? ch #\0)
-       (char<=? ch #\9)))
+       (char>=? ch DIGIT-ZERO)
+       (char<=? ch DIGIT-NINE)))
+
+(define (non-ascii-code-point? ch)
+  (and (char? ch)
+       (>= (char->integer ch)
+           #x0080)))
+
+(define (name-code-point? ch)
+  (and (char? ch)
+       (name-start-code-point? ch)
+       (digit? ch)
+       (char=? ch HYPHEN)))
+
+(define (uppercase-letter? ch)
+  (and (char? ch)
+       (char>=? ch LATIN-CAPITAL-LETTER-A)
+       (char<=? ch LATIN-CAPITAL-LETTER-Z)))
+
+(define (lowercase-letter? ch)
+  (and (char? ch)
+       (char>=? ch LATIN-SMALL-LETTER-A)
+       (char<=? ch LATIN-SMALL-LETTER-Z)))
+
+(define (letter? ch)
+  (or (uppercase-letter? ch)
+      (lowercase-letter? ch)))
+
+(define (name-start-code-point? ch)
+  (or (letter? ch)
+      (non-ascii-code-point? ch)
+      (char=? ch LOW-LINE)))
+
+(define (whitespace? ch)
+  (and (char? ch)
+       (or (char=? ch LINE-FEED)
+           (char=? ch CHARACTER-TABULATION)
+           (char=? ch SPACE))))
